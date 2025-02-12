0aa474a4c3457df662f74518fed3d4267c452d9c
Paul <paulperrot9@gmail.com>
2025-02-12 16:14:55 +0100

    v0.5

---
+++ partials/footer.php
@@ -0,0 +1,33 @@
+<footer class="container mx-auto bg-white py-8 border-t border-gray-400">
+    <div class="container flex px-3 py-8">
+        <div class="w-full mx-auto flex flex-wrap">
+            <div class="flex w-full lg:w-1/2">
+                <div class="px-3 md:px-0">
+                    <h3 class="font-bold text-gray-900">About</h3>
+                    <p class="text-xl text-gray-600 mb-8 albert-sans">
+                        Notre carte se renouvelle toute les semaines, n'hésitez pas à nous contacter ou la consulter sur le site pour en savoir plus !
+                    </p>
+                </div>
+            </div>
+            <div class="flex w-full lg:w-1/2 lg:justify-end lg:text-right mt-6 md:mt-0">
+                <div class="px-3 md:px-0">
+                    <h3 class="text-left text-gray-900 leaguegothic-bold">Nos réseaux</h3>
+                    <div class="w-full flex items-center py-4 mt-0">
+                        <a href="#" class="mx-2">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                        <a href="#" class="mx-2">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                        <a href="https://www.instagram.com/panaille_bordeaux/" class="mx-2">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                        <a href="#" class="mx-2">
+                            <i class="fab fa-youtube"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
